34d78446897b266e491f1751e5bda8699e35bfb7
Rahul Moharana <moharanarahul466@gmail.com>
2024-02-04 19:16:47 +0530

    memory understanding

---
+++ day7/memory.js
@@ -0,0 +1,14 @@
+let myemail = "moharanarahul466@gmail.com";
+let anotheremail = myemail
+anotheremail= "anotheremail@gmail.com"
+console.log(myemail);
+console.log(anotheremail);
+
+let user = {
+    email:"user.@gmail.com",
+    upi: "user",
+}
+let user2 = user;
+user2.upi = "user2";
+console.log(user2);
+console.log(user);
