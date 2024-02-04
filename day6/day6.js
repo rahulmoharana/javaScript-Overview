c04e319f14f49ab3ab0eef7e7f499db90163157f
Rahul Moharana <moharanarahul466@gmail.com>
2024-02-04 10:19:56 +0530

    day6 of js push

--- day6/day6.js
+++ day6/day6.js
@@ -1,2 +1,15 @@
 //comparesion of datatype
 
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0)//false
+console.log(null==0)//false
+console.log(null >= 0);//true
+
+console.log(undefined > 0)//false
+console.log(undefined==0)//false
+console.log(undefined >= 0);//false
+
+// === strict check.
+
